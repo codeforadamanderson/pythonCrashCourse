6a6b09dec127b183a1040a280ff2b2883d8b17e0
Adam Anderson <mailforadamanderson@gmail.com>
2020-08-11 18:13:35 +0000

    Added comments

--- Chapter2/apostrophe.py
+++ Chapter2/apostrophe.py
@@ -1,2 +1,3 @@
+# Print a message
 message = "One of Python's strenghts is its diverse community."
 print(message)
--- Chapter2/hello_world.py
+++ Chapter2/hello_world.py
@@ -1,6 +1,8 @@
+# Print one message
 message = "Hello Python world!"
 print(message)
 
+# Print another message
 message = "Hello Python Crash Course world!"
 print(message)
 
