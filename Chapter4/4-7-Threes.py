22b4a73a7c790a139edbdef6cc4e2e4cbd39b073
Adam Anderson <mailforadamanderson@gmail.com>
2021-02-09 22:42:30 -0600

    Fixed line length

--- Chapter4/4-7-Threes.py
+++ Chapter4/4-7-Threes.py
@@ -1,3 +1,4 @@
-#Make a list of the muliples of 3 from 3 to 30 and use a for loop to print the numbers.
+#Make a list of the muliples of 3 from 3 to 30
+#and use a for loop to print the numbers.
 for value in range(3,31,3):
     print(value)
